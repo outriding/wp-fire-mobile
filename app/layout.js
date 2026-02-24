2dd700e02e09150b27460536e43db22e3e099ba7
peterridingdev <peterdriding@gmail.com>
2026-02-24 10:56:07 +0400

    perf: optimize font loading using next/font

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,15 @@
+// app/layout.tsx   ← still .tsx because it contains JSX
 import './globals.css';
+import { Roboto } from 'next/font/google';
 import Header from '../components/main-header/Header';
 import Footer from '../components/Footer';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '700'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
 
 export const metadata = {
   title: 'WP Fire - Fire Alarm Installation Services',
@@ -17,8 +26,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className="overflow-x-hidden font-roboto m-0 p-0 flex 
-      justify-center items-stretch flex-col min-h-screen bg-white"
+        className={`${roboto.variable} font-roboto overflow-x-hidden m-0 p-0 flex 
+        justify-center items-stretch flex-col min-h-screen bg-white`}
       >
         <Header />
         <main>{children}</main>
